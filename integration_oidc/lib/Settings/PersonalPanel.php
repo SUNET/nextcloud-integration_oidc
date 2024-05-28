1c8033521ce272713ca992fa464f5af229d21568
Micke Nordin <kano@sunet.se>
2024-05-28 15:16:44 +0200

    Rename

---
+++ integration_oidc/lib/Settings/PersonalPanel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace OCA\IOIDC\Settings;
+
+use OCA\IOIDC\AppInfo\Application;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
+use OCP\Settings\ISettings;
+
+class PersonalSettings implements ISettings
+{
+  public function __construct(
+    private IInitialState $initialStateService,
+  ) {
+  }
+
+  /**
+   * @return TemplateResponse
+   */
+  public function getForm(): TemplateResponse
+  {
+    //$this->initialStateService->provideInitialState('admin-config', $adminConfig);
+    return new TemplateResponse(Application::APP_ID, 'personalSettings');
+  }
+
+  public function getSection(): string
+  {
+    return 'connected-accounts';
+  }
+
+  public function getPriority(): int
+  {
+    return 10;
+  }
+}
